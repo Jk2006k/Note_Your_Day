e493df1319ec381080b5cfffa151462e6f4f66af
Kishoore <kishoore004@gmail.com>
2025-06-12 15:27:14 +0530

    Note Your Day

--- Backend/Models/Login.js
+++ Backend/Models/Login.js
@@ -8,6 +8,8 @@ const LoginSchema=new mongoose.Schema({
     email:{
         type:String,
         required:true,
+        match: [/^\S+@\S+\.\S+$/, 'Please use a valid email address'],
+
     }
 })
 
